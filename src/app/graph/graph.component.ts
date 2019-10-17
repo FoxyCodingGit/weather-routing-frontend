8eab0dfdb1315d1ed00d0642cb52b02abda2683b
Adam Fox <Adam.Fox@derivco.co.uk>
2019-10-17 17:33:12 +0100

    Tell user of best route and related information

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -24,13 +24,19 @@ export class GraphComponent implements OnInit {
         }
     }]
     }
-  }
+  };
 
   public lineChartLegend = true;
 
-  public chartData = [{ data: [0, 20, 50, 100], label: 'Account A' }];
+  public chartData = [{ data: [100, 100, 80, 100], label: 'Account A' }];
 
   public chartLabels = ['5', '10', '15', '20'];
+
+  public bestRoute: {
+    name: string,
+    whenShouldleave: string,
+    lowestPercentage: number
+  };
 
   constructor() { }
 
@@ -50,11 +56,39 @@ export class GraphComponent implements OnInit {
       data: percentages,
       label: 'adamTestRee'
     });
-    this.chartColours.push({backgroundColor: 'rgba(' + randomIntFromInterval(0, 255) + ', ' + randomIntFromInterval(0, 255) +
-    ', ' + randomIntFromInterval(0, 255) + ', 0.1)'});
+    this.chartColours.push({backgroundColor: 'rgba(' + this.randomIntFromInterval(0, 255) + ', ' + this.randomIntFromInterval(0, 255) +
+    ', ' + this.randomIntFromInterval(0, 255) + ', 0.1)'});
+
+    this.updateBestRoute();
   }
+
+  private updateBestRoute(): void {
+    let lowestPercentage = 100;
+    let routeName: string;
+    let whenShoudlLeave: string;
+
+    this.chartData.forEach(hello => {
+
+      for (let i = 0; i < hello.data.length; i++) {
+        let rainProbabilty = hello.data[i];
+        if (rainProbabilty < lowestPercentage) {
+          lowestPercentage = rainProbabilty;
+          routeName = hello.label;
+          whenShoudlLeave = this.chartLabels[i];
+        }
+      }
+    });
+
+    this.bestRoute = {
+      name: routeName,
+      whenShouldleave: whenShoudlLeave,
+      lowestPercentage
+    };
+  }
+
+  private randomIntFromInterval(min, max): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
 }
 
-function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
