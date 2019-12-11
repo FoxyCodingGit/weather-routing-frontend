eb1ba72fdc621fbeefc3f7375f16b33c2fc00b8e
Adam Fox <Adam.Fox@derivco.co.uk>
2019-12-11 16:52:43 +0000

    triple equals

--- src/app/route-creation/route-creation.component.ts
+++ src/app/route-creation/route-creation.component.ts
@@ -93,29 +93,30 @@ export class RouteCreationComponent implements OnInit {
 
     return new Promise(function(resolve, reject) {
       geocoder.geocode({ 'location': latLng }, function (results) {
-        if (results[0]) {
+        let addressOutput = '';
+
+        if (!results) {
+          addressOutput = 'Geocoder passed but result null';
+        } else if (results[0]) {
           //that.zoom = 11;
           //that.currentLocation = results[0].formatted_address;
-
-          let addressOutput: string = "";
 
           console.log(results[0]);
 
           results[0].address_components.forEach(addressPart => {
-            if (addressPart.types[0] == "street_number"
-            || addressPart.types[0] == "route"
-            || addressPart.types[0] == "postal_code") {
-              addressOutput += addressPart.long_name + " ";
+            if (addressPart.types[0] === 'street_number'
+            || addressPart.types[0] === 'route'
+            || addressPart.types[0] === 'postal_code') {
+              addressOutput += addressPart.long_name + ' ';
             }
           });
 
           addressOutput = addressOutput.substring(0, addressOutput.length - 1);
 
           resolve(addressOutput);
-        }
-        else {
+        } else {
           console.log('No results found');
-          reject("Error!")
+          reject('Error!');
         }
       });
     });
